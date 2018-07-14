57160a20723c395796c78a6505e4d4bcad26ba4d
Graham Burek <graham.burek1@gmail.com>
2018-07-14 14:10:26 -0400

    Adds test isometric tiles, tilemap, and sprite.

---
+++ android/assets/Redo.tsx
@@ -0,0 +1,208 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Redo" tilewidth="64" tileheight="64" tilecount="68" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="64" height="64" source="UH_Tileset/ts_beach_straight_45.png"/>
+ </tile>
+ <tile id="1">
+  <image width="64" height="64" source="UH_Tileset/ts_beach_straight_135.png"/>
+ </tile>
+ <tile id="2">
+  <image width="64" height="64" source="UH_Tileset/ts_beach_straight_225.png"/>
+ </tile>
+ <tile id="3">
+  <image width="64" height="64" source="UH_Tileset/ts_beach_straight_315.png"/>
+ </tile>
+ <tile id="4">
+  <image width="64" height="64" source="UH_Tileset/ts_beach-shallow_curve_in_45.png"/>
+ </tile>
+ <tile id="5">
+  <image width="64" height="64" source="UH_Tileset/ts_beach-shallow_curve_in_135.png"/>
+ </tile>
+ <tile id="6">
+  <image width="64" height="64" source="UH_Tileset/ts_beach-shallow_curve_in_225.png"/>
+ </tile>
+ <tile id="7">
+  <image width="64" height="64" source="UH_Tileset/ts_beach-shallow_curve_in_315.png"/>
+ </tile>
+ <tile id="8">
+  <image width="64" height="64" source="UH_Tileset/ts_beach-shallow_curve_out_45.png"/>
+ </tile>
+ <tile id="9">
+  <image width="64" height="64" source="UH_Tileset/ts_beach-shallow_curve_out_135.png"/>
+ </tile>
+ <tile id="10">
+  <image width="64" height="64" source="UH_Tileset/ts_beach-shallow_curve_out_225.png"/>
+ </tile>
+ <tile id="11">
+  <image width="64" height="64" source="UH_Tileset/ts_beach-shallow_curve_out_315.png"/>
+ </tile>
+ <tile id="12">
+  <image width="64" height="64" source="UH_Tileset/ts_beach-shallow_straight_45.png"/>
+ </tile>
+ <tile id="13">
+  <image width="64" height="64" source="UH_Tileset/ts_beach-shallow_straight_135.png"/>
+ </tile>
+ <tile id="14">
+  <image width="64" height="64" source="UH_Tileset/ts_beach-shallow_straight_225.png"/>
+ </tile>
+ <tile id="15">
+  <image width="64" height="64" source="UH_Tileset/ts_beach-shallow_straight_315.png"/>
+ </tile>
+ <tile id="20">
+  <image width="64" height="64" source="UH_Tileset/ts_grass_straight_45.png"/>
+ </tile>
+ <tile id="21">
+  <image width="64" height="64" source="UH_Tileset/ts_grass_straight_135.png"/>
+ </tile>
+ <tile id="22">
+  <image width="64" height="64" source="UH_Tileset/ts_grass_straight_225.png"/>
+ </tile>
+ <tile id="23">
+  <image width="64" height="64" source="UH_Tileset/ts_grass_straight_315.png"/>
+ </tile>
+ <tile id="24">
+  <image width="64" height="64" source="UH_Tileset/ts_grass1_straight_45.png"/>
+ </tile>
+ <tile id="25">
+  <image width="64" height="64" source="UH_Tileset/ts_grass1_straight_135.png"/>
+ </tile>
+ <tile id="26">
+  <image width="64" height="64" source="UH_Tileset/ts_grass1_straight_225.png"/>
+ </tile>
+ <tile id="27">
+  <image width="64" height="64" source="UH_Tileset/ts_grass1_straight_315.png"/>
+ </tile>
+ <tile id="28">
+  <image width="64" height="64" source="UH_Tileset/ts_grass2_straight_45.png"/>
+ </tile>
+ <tile id="29">
+  <image width="64" height="64" source="UH_Tileset/ts_grass2_straight_135.png"/>
+ </tile>
+ <tile id="30">
+  <image width="64" height="64" source="UH_Tileset/ts_grass2_straight_225.png"/>
+ </tile>
+ <tile id="31">
+  <image width="64" height="64" source="UH_Tileset/ts_grass2_straight_315.png"/>
+ </tile>
+ <tile id="32">
+  <image width="64" height="64" source="UH_Tileset/ts_grass3_straight_45.png"/>
+ </tile>
+ <tile id="33">
+  <image width="64" height="64" source="UH_Tileset/ts_grass3_straight_135.png"/>
+ </tile>
+ <tile id="34">
+  <image width="64" height="64" source="UH_Tileset/ts_grass3_straight_225.png"/>
+ </tile>
+ <tile id="35">
+  <image width="64" height="64" source="UH_Tileset/ts_grass3_straight_315.png"/>
+ </tile>
+ <tile id="36">
+  <image width="64" height="64" source="UH_Tileset/ts_grass4_straight_45.png"/>
+ </tile>
+ <tile id="37">
+  <image width="64" height="64" source="UH_Tileset/ts_grass4_straight_135.png"/>
+ </tile>
+ <tile id="38">
+  <image width="64" height="64" source="UH_Tileset/ts_grass4_straight_225.png"/>
+ </tile>
+ <tile id="39">
+  <image width="64" height="64" source="UH_Tileset/ts_grass4_straight_315.png"/>
+ </tile>
+ <tile id="40">
+  <image width="64" height="64" source="UH_Tileset/ts_grass5_straight_45.png"/>
+ </tile>
+ <tile id="41">
+  <image width="64" height="64" source="UH_Tileset/ts_grass5_straight_135.png"/>
+ </tile>
+ <tile id="42">
+  <image width="64" height="64" source="UH_Tileset/ts_grass5_straight_225.png"/>
+ </tile>
+ <tile id="43">
+  <image width="64" height="64" source="UH_Tileset/ts_grass5_straight_315.png"/>
+ </tile>
+ <tile id="44">
+  <image width="64" height="64" source="UH_Tileset/ts_grass-beach_curve_in_45.png"/>
+ </tile>
+ <tile id="45">
+  <image width="64" height="64" source="UH_Tileset/ts_grass-beach_curve_in_135.png"/>
+ </tile>
+ <tile id="46">
+  <image width="64" height="64" source="UH_Tileset/ts_grass-beach_curve_in_225.png"/>
+ </tile>
+ <tile id="47">
+  <image width="64" height="64" source="UH_Tileset/ts_grass-beach_curve_in_315.png"/>
+ </tile>
+ <tile id="48">
+  <image width="64" height="64" source="UH_Tileset/ts_grass-beach_curve_out_45.png"/>
+ </tile>
+ <tile id="49">
+  <image width="64" height="64" source="UH_Tileset/ts_grass-beach_curve_out_135.png"/>
+ </tile>
+ <tile id="50">
+  <image width="64" height="64" source="UH_Tileset/ts_grass-beach_curve_out_225.png"/>
+ </tile>
+ <tile id="51">
+  <image width="64" height="64" source="UH_Tileset/ts_grass-beach_curve_out_315.png"/>
+ </tile>
+ <tile id="52">
+  <image width="64" height="64" source="UH_Tileset/ts_grass-beach_straight_45.png"/>
+ </tile>
+ <tile id="53">
+  <image width="64" height="64" source="UH_Tileset/ts_grass-beach_straight_135.png"/>
+ </tile>
+ <tile id="54">
+  <image width="64" height="64" source="UH_Tileset/ts_grass-beach_straight_225.png"/>
+ </tile>
+ <tile id="55">
+  <image width="64" height="64" source="UH_Tileset/ts_grass-beach_straight_315.png"/>
+ </tile>
+ <tile id="56">
+  <image width="64" height="64" source="UH_Tileset/ts_shallow_straight_45.png"/>
+ </tile>
+ <tile id="57">
+  <image width="64" height="64" source="UH_Tileset/ts_shallow_straight_135.png"/>
+ </tile>
+ <tile id="58">
+  <image width="64" height="64" source="UH_Tileset/ts_shallow_straight_225.png"/>
+ </tile>
+ <tile id="59">
+  <image width="64" height="64" source="UH_Tileset/ts_shallow_straight_315.png"/>
+ </tile>
+ <tile id="60">
+  <image width="64" height="64" source="UH_Tileset/ts_shallow-deep_curve_in_45.png"/>
+ </tile>
+ <tile id="61">
+  <image width="64" height="64" source="UH_Tileset/ts_shallow-deep_curve_in_135.png"/>
+ </tile>
+ <tile id="62">
+  <image width="64" height="64" source="UH_Tileset/ts_shallow-deep_curve_in_225.png"/>
+ </tile>
+ <tile id="63">
+  <image width="64" height="64" source="UH_Tileset/ts_shallow-deep_curve_in_315.png"/>
+ </tile>
+ <tile id="64">
+  <image width="64" height="64" source="UH_Tileset/ts_shallow-deep_curve_out_45.png"/>
+ </tile>
+ <tile id="65">
+  <image width="64" height="64" source="UH_Tileset/ts_shallow-deep_curve_out_135.png"/>
+ </tile>
+ <tile id="66">
+  <image width="64" height="64" source="UH_Tileset/ts_shallow-deep_curve_out_225.png"/>
+ </tile>
+ <tile id="67">
+  <image width="64" height="64" source="UH_Tileset/ts_shallow-deep_curve_out_315.png"/>
+ </tile>
+ <tile id="68">
+  <image width="64" height="64" source="UH_Tileset/ts_shallow-deep_straight_45.png"/>
+ </tile>
+ <tile id="69">
+  <image width="64" height="64" source="UH_Tileset/ts_shallow-deep_straight_135.png"/>
+ </tile>
+ <tile id="70">
+  <image width="64" height="64" source="UH_Tileset/ts_shallow-deep_straight_225.png"/>
+ </tile>
+ <tile id="71">
+  <image width="64" height="64" source="UH_Tileset/ts_shallow-deep_straight_315.png"/>
+ </tile>
+</tileset>
--- core/src/com/mygdx/game/TileGame.java
+++ core/src/com/mygdx/game/TileGame.java
@@ -3,31 +3,77 @@ package com.mygdx.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.IsometricTiledMapRenderer;
+import com.badlogic.gdx.math.Matrix4;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class TileGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+	private TiledMap map;
+	private IsometricTiledMapRenderer renderer;
+	private OrthographicCamera camera;
+	private Texture character;
+	private TextureRegion[] characterPoses;
+	private SpriteBatch batch;
+	private Matrix4 isoMatrix;
+	private Viewport viewport;
+
 	@Override
-	public void create () {
+	public void create() {
+
+		isoMatrix = new Matrix4().idt()
+				.scale((float) (Math.sqrt(2.0) / 2.0), (float) (Math.sqrt(2.0) / 4.0), 1.0f)
+				.scale(2,2,1) // Fixes placement problem with tilemaps
+				.rotate(0.0f, 0.0f, 1.0f, -45);
+
+
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		// Load map from Tiled and create isometric renderer
+		camera = new OrthographicCamera();
+		viewport = new ExtendViewport(20, 20, camera);
+		viewport.apply();
+
+		map = new TmxMapLoader().load("Test_Map.tmx");
+		renderer = new IsometricTiledMapRenderer(map, 1 / 32f, batch);
+
+		Vector3 vec = new Vector3(5,5, 0).mul(isoMatrix);
+		camera.translate(vec.x, vec.y);
+		camera.update();
+		// Load character spritesheet, split poses into separate textures
+		character = new Texture(Gdx.files.internal("Sprites/Characters/new-sprites.png"));
+		TextureRegion[][] splitPoses = TextureRegion.split(character, character.getWidth() / 4, character.getHeight());
+		characterPoses = new TextureRegion[4];
+		for (int i = 0; i < 4; i++) {
+			characterPoses[i] = splitPoses[0][i];
+		}
 	}
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+	public void render() {
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		camera.update();
+		batch.setProjectionMatrix(camera.combined);
+		Vector3 vec = new Vector3(1, 1, 0);
+		vec.mul(isoMatrix);
+		renderer.setView(camera);
+		renderer.render();
+		viewport.apply();
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(characterPoses[1], vec.x, vec.y + 0.5f, 2, 1);
 		batch.end();
 	}
-	
+
 	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+	public void resize(int width, int height) {
+		super.resize(width, height);
+		viewport.update(width, height);
 	}
 }
